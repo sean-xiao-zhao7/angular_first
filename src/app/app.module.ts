3d46a20724f5c661d33dbfd488051936ba98efe5
Sean Xiao Zhao <xiaofeng.zhao@utoronto.ca>
2021-09-30 12:51:59 -0400

    Complete section 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ServerComponent } from './server/server.component';
+import { Server2Component } from './server2/server2.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServerComponent,
+    Server2Component
   ],
   imports: [
     BrowserModule,
